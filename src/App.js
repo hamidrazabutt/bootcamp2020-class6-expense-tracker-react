49238953f9aa75556da1d09e2c0dc505b47ca56a
Muhammad Hamid Raza <68273560+hamidrazabutt@users.noreply.github.com>
2021-12-10 02:16:21 +0500

    New

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import './App.css';
-import Child from './Child';
-import { TransactionProvider } from './transContext';
 
 function App() {
-  return (
-    <TransactionProvider>
-      <Child />
-    </TransactionProvider>
-  );
+  return <div>Hello World</div>;
 }
 
 export default App;
