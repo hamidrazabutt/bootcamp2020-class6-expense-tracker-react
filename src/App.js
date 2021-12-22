04135776e2a29a2e2487e0834292d6d191f31db8
Muhammad Hamid Raza <68273560+hamidrazabutt@users.noreply.github.com>
2021-12-23 01:45:28 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { AddTransaction } from './components/AddTransaction';
+import { Balance } from './components/Balance';
+import { Header } from './components/Header';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
 import './App.css';
-import Child from './Child';
-import { TransactionProvider } from './transContext';
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
-    <TransactionProvider>
-      <Child />
-    </TransactionProvider>
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
