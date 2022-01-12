7d593f196cb719eda69d7c1ed9c22b192d8e95b0
Muhammad Hamid Raza <68273560+hamidrazabutt@users.noreply.github.com>
2022-01-13 01:49:59 +0500

    Expense Tracker Done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
+
+// Import CSS
 import './App.css';
-import Child from './Child';
-import { TransactionProvider } from './transContext';
+
+// Import Components
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { AccountSummary } from './components/AccountSummary';
+import { TransactionHistory } from './components/TransactionHistory';
+import { AddTransaction } from './components/AddTransaction';
+
+// Import Provider
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
-    <TransactionProvider>
-      <Child />
-    </TransactionProvider>
+      <GlobalProvider>
+        <Header />
+        <div className="container">
+          <Balance />
+          <AccountSummary />
+          <TransactionHistory />
+          <AddTransaction />
+        </div>
+      </GlobalProvider>
   );
 }
 
